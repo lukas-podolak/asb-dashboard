edad60e45105c55451b298b633aa06a927edca7f
Lukáš Podolák <50804011+lukas-podolak@users.noreply.github.com>
2025-12-03 21:41:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/asb-dashboard/',
+  base: process.env.NODE_ENV === 'production' ? '/asb-dashboard/' : '/',
 })
